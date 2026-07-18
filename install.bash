4355fc6a97f2fd24f075d6b3bf33f3f2c9025efe
PauloGabrielLeme <pggleme@gmail.com>
2026-07-18 03:05:10 -0300

    Atualização no script

--- install.bash
+++ install.bash
@@ -1,4 +1,11 @@
 #! /usr/bin/bash
 
+echo "Installing configurtion"
+
 mv Wallpapers ~/Imagens
 mv fish foot helix hypr noctalia wofi user-dirs.dirs ~/.config
+
+echo "Configuration Installed"
+echo "Deleting this folder"
+
+cd && rm -rf config
