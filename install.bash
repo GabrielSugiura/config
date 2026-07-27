d78a4768d5f69f5e31762a23e9089a18abce51b6
pggleme@gmail.com <pggleme@gmail.com>
2026-07-27 13:37:43 -0300

    Correção de erros de sintaxe e pequenas adições

--- install.bash
+++ install.bash
@@ -1,23 +1,25 @@
-#! /usr/bin/bash
+#!/usr/bin/env bash
 
-echo "---------------------------------------\n"
+echo "---------------------------------------"
 echo "Installing configurtion"
 
 
-if (( EOID == 1 )); then
+if (( EUID == 0 )); then
   echo "Executing as root"
+  echo "Copying Grub post kernel install configuration to /etc"
+  mv 90-grub /etc/kernel/postinst.d
   echo "Copying Gentoo configuration to /etc"
-  echo "---------------------------------------\n"
+  echo "---------------------------------------"
   echo "Chose a option below:\n"
-  echo "(0) Intel CPU Only"
-  echo "(1) AMD CPU + Nvidia GPU\n: "
+  printf "(0) Intel CPU Only\n"
+  printf "(1) AMD CPU + Nvidia GPU\n: "
   read option
-  if (( $option == "0" )); then
+  if (( option == 0 )); then
     rm -rf /etc/portage/*
-    mv intel-portage/* /etc/portage
-  elif (( $option == "1" )); then
+    mv portage-intel/* /etc/portage
+  elif (( option == 1 )); then
     rm -rf /etc/portage/*
-    mv amd-nvidia-portage/* /etc/portage
+    mv portage-amd-nvidia/* /etc/portage
   else
     echo "No valid option chosen, skipping..."
   fi
@@ -29,10 +31,10 @@ fi
 mv Wallpapers ~/Imagens
 mv fish foot helix hypr noctalia wofi user-dirs.dirs ~/.config
 
-echo "---------------------------------------\n"
+echo "---------------------------------------"
 echo "Configuration Installed"
 echo "Deleting this folder"
-echo "---------------------------------------\n"
+echo "---------------------------------------"
 
 cd && rm -rf config
 exit
