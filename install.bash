9f5bb71735a37ce19e26ccc08c558f2fbc9813fe
PauloGabrielLeme <pggleme@gmail.com>
2026-07-27 03:09:15 -0300

    Adição de automatização das configurações do portage

--- install.bash
+++ install.bash
@@ -1,12 +1,38 @@
 #! /usr/bin/bash
 
+echo "---------------------------------------\n"
 echo "Installing configurtion"
+
+
+if ( EOID == 1); then
+  echo "Executing as root"
+  echo "Copying Gentoo configuration to /etc"
+  echo "---------------------------------------\n"
+  echo "Chose a option below:\n"
+  echo "(0) Intel CPU Only"
+  echo "(1) AMD CPU + Nvidia GPU\n: "
+  read option
+  if ( $option == 0); then
+    rm -rf /etc/portage/*
+    mv intel-portage/* /etc/portage
+  elif ( $option == 1 ); then
+    rm -rf /etc/portage/*
+    mv amd-nvidia-portage/* /etc/portage
+  else
+    echo "No valid option chosen, skipping..."
+  fi
+
+else
+  echo "Executing as non root"
+fi
 
 mv Wallpapers ~/Imagens
 mv fish foot helix hypr noctalia wofi user-dirs.dirs ~/.config
 
+echo "---------------------------------------\n"
 echo "Configuration Installed"
 echo "Deleting this folder"
+echo "---------------------------------------\n"
 
 cd && rm -rf config
 exit
